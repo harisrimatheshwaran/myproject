ebba042ef8d67e1f108b001e5808031a09d70e71
harisrimatheshwaran <harisri170206@gmail.com>
2025-04-24 18:55:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
         server.createContext("/", new HttpHandler() {
             @Override
             public void handle(HttpExchange exchange) throws IOException {
-                String response = "Hello, World!";
+                String response = "Hello, World!!!";
                 exchange.sendResponseHeaders(200, response.getBytes().length);
                 OutputStream os = exchange.getResponseBody();
                 os.write(response.getBytes());
